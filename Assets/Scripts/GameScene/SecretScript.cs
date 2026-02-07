198e4d51927a9cf90b8b6212c9974004cf4d63bc
Roddy Rappaport <roddy.rappaport@gmail.com>
2026-02-08 01:36:47 +0200

    Middle-click for all buttons now default
    It is objectively the correct choice, and has been a feature of the game
    for ages. The secret is funny, so I don't want to entirely remove it,
    but no-one should actually... Use it...

--- Assets/Scripts/GameScene/SecretScript.cs
+++ Assets/Scripts/GameScene/SecretScript.cs
@@ -11,11 +11,19 @@ public class SecretScript : MonoBehaviour
     private byte timesClicked;
 
     public static bool allMiddleClick;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        allMiddleClick = PlayerPrefs.GetInt("AllMiddleClick") == 1;
+        const string middleClickKey = "AllMiddleClick";
+
+        // Set default to allow middle-click for all buttons by default.
+        if (!PlayerPrefs.HasKey(middleClickKey))
+        {
+            PlayerPrefs.SetInt(middleClickKey, 1);
+        }
+
+        allMiddleClick = PlayerPrefs.GetInt(middleClickKey) == 1;
         timesClicked = 0;
     }
 
@@ -28,13 +36,13 @@ public class SecretScript : MonoBehaviour
             if (!allMiddleClick)
             {
                 allMiddleClick = true;
-                Debug.Log("Secret activated! all mouse buttons now function like a middle click!");
-                FlashingTextScript.statusText.DisplayText("Secret activated! all mouse buttons now also function as middle click!", 0.2f, 5);
+                Debug.Log("Secret deactivated! all mouse buttons now function like a middle click!");
+                FlashingTextScript.statusText.DisplayText("Secret deactivated! all mouse buttons now also function as middle click!", 0.2f, 5);
             }
             else
             {
                 allMiddleClick = false;
-                FlashingTextScript.statusText.DisplayText("Secret deactivated! Only middle click will reveal adjacent tiles!", 0.2f, 5);
+                FlashingTextScript.statusText.DisplayText("Secret activated! Only middle click will reveal adjacent tiles!", 0.2f, 5);
             }
             PlayerPrefs.SetInt("AllMiddleClick", allMiddleClick? 1 : 0);
         }
